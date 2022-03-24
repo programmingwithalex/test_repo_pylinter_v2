13644654a0f304ca684f5d1dc23017b0746eb30e
jonatas <jonatasolemos@gmail.com>
2022-03-24 17:08:44 -0300

    ok

--- temp/main2.py
+++ temp/main2.py
@@ -12,3 +12,11 @@ def word_count(sentence, word) -> int:
         return sum([1 for x in sentence if x == word])
     else:
         return 0
+
+
+def word_count_two(sentence, word) -> int:
+    sentence = sentence.upper().split()
+    if word in sentence:
+        return sum([1 for x in sentence if x == word])
+    else:
+        return 0
