a44c5a019131b008ada6c06e97e33369d14dbb9b
kryvetskyi <kryvetskyi@gmail.com>
2023-05-05 16:15:37 +0300

    fix mypy 3

--- test/temporary/test_main2.py
+++ test/temporary/test_main2.py
@@ -1,24 +1,23 @@
-from temp import temporary
+from temp import sub_and_word_count
 import pytest
 
 
 @pytest.mark.parametrize('x, y, expected',
-    [
-        (3, 4, -1),
-        (4.5, 4, 0),
-        (3.9, 4, -1),
-        (4.2, 3.9, 0)
-    ])
+                         [
+                             (3, 4, -1),
+                             (4.5, 4, 0),
+                             (3.9, 4, -1),
+                             (4.2, 3.9, 0)
+                         ])
 def test_sub_parametrize(x, y, expected):
-    assert temporary.sub(x, y) == expected
+    assert sub_and_word_count.sub(x, y) == expected
 
 
 @pytest.mark.parametrize('sentence, word, expected_count',
-    [
-        ('arm pod race', 'pod', 1),
-        ('arm pod race', 'lap', 0),
-        ('arm arm arm', 'arm', 3),
-    ])
+                         [
+                             ('arm pod race', 'pod', 1),
+                             ('arm pod race', 'lap', 0),
+                             ('arm arm arm', 'arm', 3),
+                         ])
 def test_word_count(sentence, word, expected_count):
-    assert temporary.word_count(sentence, word) == expected_count
-    
+    assert sub_and_word_count.word_count(sentence, word) == expected_count
